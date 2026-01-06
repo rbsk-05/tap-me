b439e653a900eaff8f5d4beb6fa0582642c665d4
Darshan-M05 <230701061@rajalakshmi.edu.in>
2026-01-06 18:56:33 +0530

    Update project code

--- tap-me/src/App.jsx
+++ tap-me/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const addTap = () => {
+    const tapCount = document.getElementById('tap-count');
+    let currentTap = parseInt(tapCount.innerText);
+    currentTap += 1;
+    tapCount.innerText = currentTap;
+    console.log(currentTap);
+  }
+
+  const refreshTap = () => {
+    const tapCount = document.getElementById('tap-count');
+    tapCount.innerText = 0;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="div-header">TAP ME!</div>
+      <p id="sub-header">My First Tapping Project</p>
+
+      <div id="tap-header">
+        <div>Total Taps</div>
+        <div id="tap-count">0</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      
+      <div id="div-button-header">
+         <button id="tap-button" onClick={addTap}>TAP HERE</button>
+      <button id="tap-refresh-button" onClick={refreshTap}>TAP FRESH</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div id="div-footer">
+        <p>💛 Made by Darshan | RBSK</p>
+        <img src="darshan.jpg" alt="My photo"></img>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
